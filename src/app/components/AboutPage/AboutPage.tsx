74895463a9c34d89322ad7321bf6547dd0a909bc
jsoranno15 <jsoranno15@gmail.com>
2024-07-25 18:40:26 -0400

    test pages

---
+++ src/app/components/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <main className="flex flex-col gap-3">
+      <div>This is the about page</div>
+      <div>info item 1</div>
+      <div> info item 2</div>
+    </main>
+  );
+};
+
+export default AboutPage;

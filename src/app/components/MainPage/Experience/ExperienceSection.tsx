b0ca91a38b540e6b8df4b04a0e1af5c078392339
jsoranno15 <jsoranno15@gmail.com>
2024-08-24 18:53:29 -0400

    fixed experience section

--- src/app/components/MainPage/Experience/ExperienceSection.tsx
+++ src/app/components/MainPage/Experience/ExperienceSection.tsx
@@ -13,30 +13,38 @@ export const ExperienceSection = () => {
         {/* <div className="w-[600px] blur-3xl z-[0] relative ">
           <div className="aspect-ratio-square z-[-2] absolute translate-x-20 -translate-y-[200px] bg-[#EA00D930] h-[300px] w-[400px] rounded-full"></div>
         </div> */}
-        <div className="flex flex-col gap-[50px] ">
+        <div className="flex flex-col gap-[50px]  ">
           {Experience.map((item, index) => {
             return (
               <div
                 key={index}
                 className="from-gray-800 z-[3]  blur-2 bg-gradient-to-b rounded-xl p-[1px]"
               >
-                <div className="flex-col flex md:flex-row gap-4 md:gap-[30px]   bg-gradient-to-t from-[#12161e]  from-65% to-[#66FCF110]  rounded-xl py-6 p-6 ">
+                <div className="flex-col flex md:flex-row gap-4 md:gap-[30px] rounded-xl  bg-[#12161e] p-6 ">
                   <ImageHeader image={item.image} />
 
-                  <div className=" flex flex-col 6 gap-3">
+                  <div className=" flex justify-center flex-col 6 gap-3">
                     <div>
                       {item.link ? (
-                        <a
-                          href={item.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="flex sm:text-xl gap-1 items-center font-bold transition-all duration-200 hover:text-ds-main-accent"
-                        >
-                          {item.title}
-                          <div className="h-7 min-h-7 min-w-7  w-7">
-                            <ArrowIcon />
-                          </div>
-                        </a>
+                        <div className="flex flex-col gap-1">
+                          <a
+                            href={item.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="flex sm:text-xl gap-1 items-center font-bold transition-all duration-200 hover:text-ds-main-accent"
+                          >
+                            {item.title}
+                            <div className="h-7 min-h-7 min-w-7  w-7">
+                              <ArrowIcon />
+                            </div>
+                          </a>
+                          <span className="text-ds-secondary-accent flex gap-1 items-center text-sm sm:text-base font-normal">
+                            <div className="flex h-4 w-4">
+                              <CalendarIcon />
+                            </div>
+                            {item.timeFrame}
+                          </span>
+                        </div>
                       ) : (
                         <div className="flex flex-col  gap-1 ">
                           <div className="sm:text-xl font-bold">
--- src/app/components/MainPage/Testimonials/TestimonialSection.tsx
+++ src/app/components/MainPage/Testimonials/TestimonialSection.tsx
@@ -10,15 +10,15 @@ export const TestimonialSection = () => {
       className="flex py-[40px] items-center w-full  flex-col gap-8 "
     >
       <SectionHeader text={"Testimonials"} />
-      <div className="flex flex-col gap-8 sm:gap-20 ">
+      <div className="flex flex-col  gap-8 sm:gap-20 ">
         {Testimonials.map((item, i) => {
           return (
             <div
               key={i}
               className="from-gray-800 z-[3]  blur-2 bg-gradient-to-b rounded-xl p-[1px]"
             >
-              <div className="flex flex-col bg-[#12161e] py-6 p-4 rounded-xl  gap-5">
-                <div className="flex gap-4 items-center">
+              <div className="flex flex-col bg-[#12161e]  py-6 p-4 rounded-xl  gap-5">
+                <div className="flex gap-4 items-center ">
                   <div
                     className="aspect-square rounded-full h-[60px] w-[60px]
                   min-h-[60px] min-w-[60px] p-[1.5px] bg-gradient-to-b from-ds-main-accent to-[#EA00D9]"

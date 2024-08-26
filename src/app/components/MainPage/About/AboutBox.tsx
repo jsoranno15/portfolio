64dc4dcdf360f1dd539ae0392b3a231c598db1c6
jsoranno15 <jsoranno15@gmail.com>
2024-08-26 17:37:51 -0400

    aded box

---
+++ src/app/components/MainPage/About/AboutBox.tsx
@@ -0,0 +1,53 @@
+import { GearIcon, MapIcon, PersonIcon, PersonIcon2 } from "../../icons";
+import Image from "next/image";
+
+export const AboutBox = () => {
+  const className =
+    "bg-gradient-to-r from-ds-white  to-ds-cyan text-transparent bg-clip-text w-fit flex flex-row gap-2 justify-center  ";
+  return (
+    <div
+      className="aspect-square transition-all duration-150 size-[350px] overflow-hidden 
+               lg:size-[400px] bg-dark-purple border-[1px] border-gray-800 border-opacity-60 rounded-xl"
+      style={{
+        backgroundSize: "80px 80px;",
+        backgroundImage:
+          " linear-gradient(to right, #14133A 1px, transparent 1px), linear-gradient(to bottom, #14133A 1px, transparent 1px)",
+      }}
+    >
+      <div
+        className="w-full h-full  "
+        style={{
+          background: "radial-gradient(transparent, #0D0121)",
+        }}
+      >
+        <Image
+          src={`/img/deco/deco_gradient.png`}
+          height={200}
+          width={200}
+          alt="Picture of the author"
+          className="rotate-90 opacity-50 z-[1] object-cover absolute rounded-t-xl size-[350px] lg:size-[400px]"
+        />
+        <div className="flex  w-full items-center text-center z-[2] flex-col p-8 text-3xl font-bold gap-[50px]  justify-center h-full">
+          <span className={`${className} `}>
+            <div className="size-8 text-ds-white ">
+              <PersonIcon2 />
+            </div>
+            Juliana Soranno
+          </span>
+          <span className={className}>
+            <div className="size-8 text-ds-white ">
+              <GearIcon />
+            </div>
+            Full Stack Engineer
+          </span>
+          <span className={className}>
+            <div className="size-8 text-ds-white ">
+              <MapIcon />
+            </div>
+            Based in New York
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/components/MainPage/About/AboutSection.tsx
+++ src/app/components/MainPage/About/AboutSection.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { SectionHeader } from "../../core/SectionHeader";
 import { useRouter } from "next/router";
-import { ArrowIcon, NormalArrowIcon, PersonIcon2 } from "../../icons";
+import { ArrowIcon, GearIcon, MapIcon, PersonIcon2 } from "../../icons";
+import { AboutBox } from "./AboutBox";
 
 export const AboutSection = () => {
   const router = useRouter();
@@ -31,25 +32,28 @@ export const AboutSection = () => {
     }
   }, [router.pathname]);
 
+  const className =
+    "bg-gradient-to-r from-ds-cyan  to-ds-main-accent text-transparent bg-clip-text w-fit flex flex-row gap-2 justify-center  ";
+
   return (
     <section
       id="section-about"
       className="flex  w-full flex-col py-[40px] sm:pb-[80px] p-4 min-h-[calc(100vh-104px)] 
       sm:px-[48px] text-sm sm:text-base  justify-center items-center gap-14  "
     >
-      <span
-        className="hidden md:inline text-[72px] lg:text-[96px] lg:leading-[96px] w-full 
-      text-center py-14 font-black text-ds-main-accent transition-all duration-150"
+      {/* <span
+        className="hidden md:inline text-[72px] lg:text-[72px] lg:leading-[72px] w-full 
+      text-center bg-gradient-to-r from-ds-cyan  to-ds-main-accent text-transparent bg-clip-text font-black  transition-all duration-150"
       >
         Juliana Soranno
-      </span>
+      </span> */}
       <div className="w-full flex gap-8 xl:max-w-[85%] text-ds-grey flex-col justify-center ">
         <div className="flex flex-row gap-8">
-          <div className="flex relative flex-col font-medium gap-5 md:w-1/2 ">
+          <div className="flex relative flex-col font-medium gap-5 lg:w-1/2 ">
             <div className="size-full md:size-[500px] absolute z-[-1]  bg-ds-main-accent opacity-10 rounded-full blur-[100px]"></div>
 
             <div className="w-full  justify-center gap-2 flex flex-col">
-              <div className="md:text-ds-main-accent text-ds-grey md:font-bold text-lg  md:hidden flex justify-start ">
+              <div className="md:text-ds-main-accent text-ds-grey  md:font-bold text-lg   md:hidden flex justify-start ">
                 Juliana Soranno
               </div>
               <SectionHeader one={"About"} two={"Me"} />
@@ -61,32 +65,34 @@ export const AboutSection = () => {
                 Full Stack Engineer
               </span>{" "}
               with a unique background in video games and technical design,
-              I&apos;ve spent the last four years honing my skills in Web3 and
-              AI technologies. Based in New York City, I specialize in crafting
-              responsive and intuitive user interfaces using{" "}
+              I&apos;ve spent four years refining my skills in{" "}
+              <span className="text-ds-main-accent font-bold"> Web3 </span> and
+              AI technologies. Based in New York City, I specialize in building
+              responsive user interfaces with{" "}
               <span className="text-ds-main-accent font-bold">
                 {" "}
                 Next.js, React, TypeScript, Zustand, and Tailwind CSS.
               </span>{" "}
-              My journey in software engineering has been driven by a passion
-              for innovation and a relentless commitment to excellence.
             </span>
             <span>
-              With experience contributing to a Web3 application that served
-              over 30,000 users, I&apos;m known for my ability to rapidly adapt
-              to new technologies and optimize user experiences under tight
-              deadlines. I excel in{" "}
-              <span className="font-bold text-ds-main-accent">
-                cross-functional environments,
-              </span>{" "}
-              where I can mentor and learn from colleagues and drive team
-              success across all project stages—from initial onboarding to
-              complex integrations.
+              I played a key role in developing a cutting-edge
+              <span className="text-ds-main-accent font-bold">
+                {" "}
+                blockchain{" "}
+              </span>
+              application that scaled to 30,000 users. This sharpened my ability
+              to quickly master new technologies and
+              <span className="text-ds-main-accent font-bold">
+                {" "}
+                deliver under pressure.{" "}
+              </span>
+              I thrive in dynamic teams, optimizing user experiences and driving
+              success.
             </span>
             <span>
               If you&apos;re interested in collaborating on challenging projects
-              or discussing the latest in front-end development, I&apos;d be
-              happy to connect.
+              or discussing the latest in technology, I&apos;d be happy to
+              connect.
             </span>
             <div className="flex flex-row w-full items-center gap-5">
               <button
@@ -115,8 +121,8 @@ export const AboutSection = () => {
             </div>
           </div>
 
-          <div className=" sm:justify-end  sm:w-1/2 sm:items-center hidden md:flex">
-            <div className="aspect-square transition-all duration-150 size-[350px]  lg:size-[400px] bg-purple-300"></div>
+          <div className=" sm:justify-end  sm:w-1/2 sm:items-center hidden lg:flex">
+            <AboutBox />
           </div>
         </div>
       </div>

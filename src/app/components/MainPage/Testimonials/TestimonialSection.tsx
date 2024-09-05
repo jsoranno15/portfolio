9089562a4023701a84da6b7b6f2f742db48d9680
jsoranno15 <jsoranno15@gmail.com>
2024-09-05 00:24:31 -0400

    improved testimonal cards

--- src/app/components/MainPage/Testimonials/TestimonialSection.tsx
+++ src/app/components/MainPage/Testimonials/TestimonialSection.tsx
@@ -14,10 +14,28 @@ export const TestimonialSection = () => {
         <div className="flex flex-col  gap-8 sm:gap-10 ">
           {Testimonials.map((item, i) => {
             return (
-              <div key={i} className="">
+              <div
+                key={i}
+                className="relative rounded-xl"
+                style={{
+                  backgroundSize: "80px 80px",
+                  backgroundImage:
+                    " linear-gradient(to right, #14133A 1px, transparent 1px), linear-gradient(to bottom, #14133A 1px, transparent 1px)",
+                }}
+              >
+                <Image
+                  src={`/img/deco/deco_gradient.png`}
+                  height={200}
+                  width={200}
+                  alt="Picture of the author"
+                  className="rotate-90 opacity-50 z-[1] right-0 absolute rounded-t-xl size-[350px] lg:size-[400px]"
+                />
                 <div
                   className="flex flex-col bg-dark-purple  py-6 p-4 rounded-xl  gap-5
               border-[1px] border-gray-800 border-opacity-60 "
+                  style={{
+                    background: "radial-gradient(transparent, #0D0121)",
+                  }}
                 >
                   <div className="flex gap-4 items-center">
                     <div

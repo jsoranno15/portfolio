4bcbbc73a0cab42fd259652fe73135d6173b60ce
jsoranno15 <jsoranno15@gmail.com>
2024-09-06 13:19:36 -0400

    removed .js from react

--- src/app/components/MainPage/Experience/Experience.tsx
+++ src/app/components/MainPage/Experience/Experience.tsx
@@ -3,9 +3,9 @@ export const Experience = [
     timeFrame: "Jan 2024 — July 2024",
     title: "Frontend Engineer — Everyrealm",
     points: [
-      "Architected and maintained the front end of Everyworld, a high-performance Web3 application serving 30,000 active users, using React.js, Next.js, TypeScript, Zustand, and Tailwind CSS.",
+      "Architected and maintained the front end of Everyworld, a high-performance Web3 application serving 30,000 active users, using React, Next.js, TypeScript, Zustand, and Tailwind CSS.",
       "Executed comprehensive testing, debugging, and deployment of a dynamic codebase with numerous evolving features, ensuring seamless adaptation and continuous improvement using GitHub for version control and Vercel for deployment.",
-      "Translated complex Figma designs into responsive, reusable React.js components, integrating features like account settings, crypto wallet connection, home pages, landing pages, profile pages, pop ups, and player tier/badge systems.",
+      "Translated complex Figma designs into responsive, reusable React components, integrating features like account settings, crypto wallet connection, home pages, landing pages, profile pages, pop ups, and player tier/badge systems.",
       "Collaborated with backend developers to integrate AWS Lambda, DynamoDB, and API Gateway, significantly boosting application performance and scalability.",
     ],
     skills: [
